1a5425292c4cb86388af280f90bf4e547bc3a217
Jane <jenya.kissel@gmail.com>
2026-04-23 12:36:16 +0200

    Initial commit

---
+++ Epsilon_Greedy.py
@@ -0,0 +1,12 @@
+# Importing Dependencies
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Epsilon Greedy Algorithm
+def EpsilonGreedy():
+    def __init__(self, epsilon, count, values):
+        self.epsilon = epsilon
+        self.count = count
+        self.values = values
+    return
