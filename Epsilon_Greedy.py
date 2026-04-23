22560f8646fb432127d08be718b4050502d46b63
Jane <jenya.kissel@gmail.com>
2026-04-23 12:55:41 +0200

    Define a methid for selecting the arm / random action

--- Epsilon_Greedy.py
+++ Epsilon_Greedy.py
@@ -6,7 +6,12 @@ import matplotlib.pyplot as plt
 # Epsilon Greedy Algorithm
 class EpsilonGreedy:
     def __init__(self, epsilon, count, values):
-        self.epsilon = epsilon
-        self.count = count
-        self.values = values
-    return
+        self.epsilon = epsilon # Exploration rate, a.k.a the frequency with which one of the options will be chosen (explore/exploit)
+        self.count = count # Number of times each action has been taken / option has been chosen
+        self.values = values # Estimated performance value of each action / option
+
+    def select_arm(self):
+        if np.random.random() > self.epsilon:
+            return ind_max(self.values) # Exploit: choose the action with the highest estimated value
+        else:
+            return np.random.randrange(len(self.values)) # Explore: choose a random action
