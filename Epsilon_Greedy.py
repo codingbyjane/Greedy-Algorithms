55dad3798ae6c2308ce6c7ff85896f699cd1edb1
Jane <jenya.kissel@gmail.com>
2026-04-23 12:57:26 +0200

    Define a function to find the maximum index value in a list

--- Epsilon_Greedy.py
+++ Epsilon_Greedy.py
@@ -10,8 +10,12 @@ class EpsilonGreedy:
         self.count = count # Number of times each action has been taken / option has been chosen
         self.values = values # Estimated performance value of each action / option
 
+    def ind_max(self, x): # Function to find the index of the maximum value in a list
+        m = max(x)
+        return x.index(m) # Return the index of the maximum value in the list
+
     def select_arm(self):
         if np.random.random() > self.epsilon:
-            return ind_max(self.values) # Exploit: choose the action with the highest estimated value
+            return self.ind_max(self.values) # Exploit: choose the action with the highest estimated value
         else:
             return np.random.randrange(len(self.values)) # Explore: choose a random action
