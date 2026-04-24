7121a5d0394d79c0e79e0deb188740b883a12fb7
Jane <jenya.kissel@gmail.com>
2026-04-24 15:41:26 +0200

    Add explanation comments

--- Epsilon_Greedy.py
+++ Epsilon_Greedy.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 # Epsilon Greedy Algorithm
 class EpsilonGreedy:
     def __init__(self, epsilon, count, values):
-        self.epsilon = epsilon # Exploration rate, a.k.a the frequency with which one of the options will be chosen (explore/exploit)
+        self.epsilon = epsilon # Exploration rate, a.k.a the frequency with which one of the options will be chosen (explore/exploit). A number between 0 and 1, where 0 means always exploit and 1 means always explore.
         self.count = count # Number of times each action has been taken / option has been chosen
-        self.values = values # Estimated performance value of each action / option
+        self.values = values # Estimated performance value of each action / option (estimated reward value)
 
-    def ind_max(self, x): # Function to find the index of the maximum value in a list
-        m = max(x)
-        return x.index(m) # Return the index of the maximum value in the list
+    def ind_max(self, x): # Function to find the index of the maximum value in a list, a.k.a, the index of the best performing arm / option
+        max_value = max(x)
+        return x.index(max_value) # Return the index of the maximum value in the list. This is used to select the action with the highest estimated value during exploitation.
 
     def select_arm(self):
         if np.random.random() > self.epsilon:
